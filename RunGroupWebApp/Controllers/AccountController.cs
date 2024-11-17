37cd195d97d8f31fe491f6253d1bd86b956dfe18
yeeydu <yeeysonduarte@gmail.com>
2024-11-17 11:32:45 +0000

    finish account register controller

--- RunGroupWebApp/Controllers/AccountController.cs
+++ RunGroupWebApp/Controllers/AccountController.cs
@@ -86,7 +86,7 @@ namespace RunGroupWebApp.Controllers
                 await _userManager.AddToRoleAsync(newUser, UserRoles.User);
             }
 
-            return View("Home");
+            return RedirectToAction("Index", "Home");
         }
 
 
